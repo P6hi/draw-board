da12c8b81adeaaf4db21dce2e2f01c1e56bff567
Arthur <a-murk@mail.ee>
2022-05-21 14:58:41 +0300

    Convert grid creation loop to function

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const container = document.getElementById("container");
 
-for (i = 0; i < 16; i++) {
+function gridCreator(gridNumber) {
+    for (i = 0; i < gridNumber; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
     container.appendChild(row);
 
-        for (j = 0; j < 16; j++) {
+        for (j = 0; j < gridNumber; j++) {
             const block = document.createElement("div");
             block.classList.add("block");
             row.appendChild(block);
         }
 }
+}
+
+gridCreator(16);
